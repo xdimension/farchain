e09b51f3ce95ae33b3f909486ef9b37b78cf40aa
xdimension <xdim222@gmail.com>
2024-10-08 12:15:11 +0700

    updated: use vrf 2.5

--- contracts/FarBuyCoupon.sol
+++ contracts/FarBuyCoupon.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.8.20;
 
-import "@chainlink/contracts/src/v0.8/vrf/VRFV2WrapperConsumerBase.sol";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
-contract FarBuyCoupon is VRFV2WrapperConsumerBase {
-
-    address owner;
+contract FarBuyCoupon is VRFConsumerBaseV2Plus {
+    // address owner;
 
     struct Ticket {
         uint32 couponID;
@@ -17,59 +17,70 @@ contract FarBuyCoupon is VRFV2WrapperConsumerBase {
     }
 
     struct Coupon {
-      uint32 couponID;
-      uint8 minNumOfTickets;
-      uint8 maxNumOfTickets;
-      uint8 numOfWinners;
-      bool isDrawn;
+        uint32 couponID;
+        uint8 minNumOfTickets;
+        uint8 maxNumOfTickets;
+        uint8 numOfWinners;
+        bool isDrawn;
     }
 
     event WinnersChosen(uint32 indexed couponID);
 
     uint32[] public couponIDs;
-    mapping(uint32 => Coupon) public coupons;  // couponID => Coupon
-    mapping(uint32 => mapping(uint32 => Ticket)) public tickets;  // couponID => ticketNum => Ticket
-    mapping(uint32 => uint32[]) public ticketNums;  // couponID => array of ticketNum
+    mapping(uint32 => Coupon) public coupons; // couponID => Coupon
+    mapping(uint32 => mapping(uint32 => Ticket)) public tickets; // couponID => ticketNum => Ticket
+    mapping(uint32 => uint32[]) public ticketNums; // couponID => array of ticketNum
 
     uint public ticketPrice;
 
     bool public isChoosingWinners;
 
     // VRF variables
-    uint32 public callbackGasLimit;
-    uint16 constant REQUEST_CONFIRMATIONS = 3;
-    uint public lastRequestId;
+    uint256 vrf_subscriptionId;
+    bytes32 public vrf_keyHash;
+    uint32 public vrf_callbackGasLimit = 1_000_000;
+    bool public vrf_nativePayment = false;
+    uint16 constant VRF_REQUEST_CONFIRMATIONS = 3;
+    uint public vrf_lastRequestId;
+
     mapping(uint => uint32) randomizeRequests; // requestId => couponID
 
     constructor(
         uint _ticketPrice,
-        address _linkAddress,
-        address _wrapperAddress,
-        uint32 _callbackGasLimit
-    )
-    VRFV2WrapperConsumerBase(
-        _linkAddress,              // LINK token address
-        _wrapperAddress            // VRF wrapper
-    )
-    {
-        owner = msg.sender;
+        uint256 _subscriptionId,
+        address _vrfCoordinator,
+        bytes32 _keyHash
+    ) VRFConsumerBaseV2Plus(_vrfCoordinator) {
+        // owner = msg.sender;
 
         ticketPrice = _ticketPrice;
 
-        callbackGasLimit = _callbackGasLimit;
+        vrf_subscriptionId = _subscriptionId;
+        vrf_keyHash = _keyHash;
     }
 
-    function createCoupon(uint32 _couponID, uint8 _minNumOfTickets, uint8 _maxNumOfTickets, uint8 _numOfWinners) public onlyOwner {
-        require(_minNumOfTickets <= _maxNumOfTickets, "Min number of tickets must be less than or equal to max number of tickets");
-        require(_numOfWinners <= _maxNumOfTickets, "Number of winners must be less than or equal to max number of tickets");
+    function createCoupon(
+        uint32 _couponID,
+        uint8 _minNumOfTickets,
+        uint8 _maxNumOfTickets,
+        uint8 _numOfWinners
+    ) public onlyOwner {
+        require(
+            _minNumOfTickets <= _maxNumOfTickets,
+            "Min number of tickets must be less than or equal to max number of tickets"
+        );
+        require(
+            _numOfWinners <= _maxNumOfTickets,
+            "Number of winners must be less than or equal to max number of tickets"
+        );
 
         coupons[_couponID] = Coupon(
-                                    _couponID,
-                                    _minNumOfTickets,
-                                    _maxNumOfTickets,
-                                    _numOfWinners,
-                                    false
-                                  );
+            _couponID,
+            _minNumOfTickets,
+            _maxNumOfTickets,
+            _numOfWinners,
+            false
+        );
     }
 
     function getCoupon(uint32 _couponID) public view returns (Coupon memory) {
@@ -79,26 +90,36 @@ contract FarBuyCoupon is VRFV2WrapperConsumerBase {
     function createTicket(uint32 _couponID) public payable {
         require(msg.value == ticketPrice, "Incorrect ticket price");
         require(coupons[_couponID].couponID != 0, "Invalid Coupon ID");
-        require(ticketNums[_couponID].length < coupons[_couponID].maxNumOfTickets, "Maximum ticket limit reached");
+        require(
+            ticketNums[_couponID].length < coupons[_couponID].maxNumOfTickets,
+            "Maximum ticket limit reached"
+        );
 
-        uint32 ticketNum = uint32(uint256(keccak256(abi.encodePacked(owner, block.timestamp))));
+        uint32 ticketNum = uint32(
+            uint256(keccak256(abi.encodePacked(owner(), block.timestamp)))
+        );
 
         tickets[_couponID][ticketNum] = Ticket(
-                                            _couponID,
-                                            ticketNum,
-                                            block.timestamp,
-                                            msg.sender,
-                                            false
-                                          );
+            _couponID,
+            ticketNum,
+            block.timestamp,
+            msg.sender,
+            false
+        );
 
         ticketNums[_couponID].push(ticketNum);
     }
 
-    function getTicket(uint32 _couponID, uint32 _ticketNum) public view returns (Ticket memory) {
+    function getTicket(
+        uint32 _couponID,
+        uint32 _ticketNum
+    ) public view returns (Ticket memory) {
         return tickets[_couponID][_ticketNum];
     }
 
-    function getTicketsByCouponID(uint32 _couponID) public view returns (Ticket[] memory) {
+    function getTicketsByCouponID(
+        uint32 _couponID
+    ) public view returns (Ticket[] memory) {
         uint32 countTickets = uint32(ticketNums[_couponID].length);
         Ticket[] memory ticketList = new Ticket[](countTickets);
 
@@ -110,22 +131,40 @@ contract FarBuyCoupon is VRFV2WrapperConsumerBase {
         return ticketList;
     }
 
-    function generateRandomNumbers(uint32 couponID, uint8 countRandomNums) internal virtual {
-        uint requestId = requestRandomness(callbackGasLimit, REQUEST_CONFIRMATIONS, countRandomNums);
-        lastRequestId = requestId;
+    function generateRandomNumbers(
+        uint32 couponID,
+        uint8 countRandomNums
+    ) internal virtual {
+
+        uint requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: vrf_keyHash,
+                subId: vrf_subscriptionId,
+                requestConfirmations: VRF_REQUEST_CONFIRMATIONS,
+                callbackGasLimit: vrf_callbackGasLimit,
+                numWords: countRandomNums,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+        vrf_lastRequestId = requestId;
         randomizeRequests[requestId] = couponID;
     }
 
-    function fulfillRandomWords(uint requestId, uint256[] memory randomness) internal override {
+    function fulfillRandomWords(
+        uint requestId,
+        uint256[] calldata randomness
+    ) internal override {
         require(randomness[0] != 0, "Problem in getting randomness");
 
         uint32 couponID = randomizeRequests[requestId];
 
-        for(uint8 i; i < coupons[couponID].numOfWinners; i++) {
-          uint idx = randomness[i] % ticketNums[couponID].length;
-          uint32 ticketNum = ticketNums[couponID][idx];
+        for (uint8 i = 0; i < coupons[couponID].numOfWinners; i++) {
+            uint idx = randomness[i] % ticketNums[couponID].length;
+            uint32 ticketNum = ticketNums[couponID][idx];
 
-          tickets[couponID][ticketNum].isWinner = true;
+            tickets[couponID][ticketNum].isWinner = true;
         }
 
         // set isDrawn flag to avoid drawing again for the this coupon
@@ -140,19 +179,30 @@ contract FarBuyCoupon is VRFV2WrapperConsumerBase {
     function chooseWinners(uint32 couponID) public onlyOwner {
         require(coupons[couponID].couponID != 0, "Invalid Coupon ID");
         require(!coupons[couponID].isDrawn, "Coupon already been drawn");
-        require(ticketNums[couponID].length >= coupons[couponID].minNumOfTickets, "Not enough tickets for the drawing");
+        require(
+            ticketNums[couponID].length >= coupons[couponID].minNumOfTickets,
+            "Not enough tickets for the drawing"
+        );
 
         isChoosingWinners = true;
 
         generateRandomNumbers(couponID, coupons[couponID].numOfWinners);
     }
 
-    function setCallbackGasLimit(uint32 _val) external onlyOwner {
-        callbackGasLimit = _val;
+    function setVRFCallbackGasLimit(uint32 _val) external onlyOwner {
+        vrf_callbackGasLimit = _val;
     }
 
-    modifier onlyOwner() {
-      require(msg.sender == owner);
-      _;
+    function setVRFNativePayment(bool _val) external onlyOwner {
+        vrf_nativePayment = _val;
     }
+
+    // function owner() public view returns(address) {
+    //   return owner;
+    // }
+
+    // modifier onlyOwner() {
+    //   require(msg.sender == owner);
+    //   _;
+    // }
 }
