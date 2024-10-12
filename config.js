1d9bdb9799a3ecabc55b49a458d72657815f216a
xdimension <xdim222@gmail.com>
2024-10-12 11:25:42 +0700

    update configs

--- config.js
+++ config.js
@@ -4,7 +4,5 @@ exports.config = {
 
     ticketPrice: 0.0001,            // The price of 1 ticket to join
 
-    VRFSubscriptionId: process.env['VRF_SUBSCRIPTION_ID'],  // VRF Subscription Id
-    VRFCoordinator: process.env['VRF_COORDINATOR'],         // VRF Coordinator address
-    VRFKeyHash: process.env['VRF_KEY_HASH'],                // VRF Key Hash
+    VRFSubscriptionId: process.env.VRF_SUBSCRIPTION_ID,  // VRF Subscription Id
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,35 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+        optimizer: {
+            enabled: true,
+            runs: 200,
+        },
+    },
+},
+networks: {
+    hardhat: {
+        // Hardhat network configuration
+    },
+
+    'base-mainnet': {
+      url: 'https://mainnet.base.org',
+      accounts: [process.env.ACCT_PRIVATE_KEY],
+      gasPrice: 1000000000,
+      VRFCoordinator: '0xd5D517aBE5cF79B7e95eC98dB0f0277788aFF634',
+      VRFKeyHash: '0xdc2f87677b01473c763cb0aee938ed3341512f6057324a584e5944e786144d70',
+    },
+
+    'base-sepolia': {
+      url: 'https://sepolia.base.org',
+      accounts: [process.env.ACCT_PRIVATE_KEY],
+      gasPrice: 1000000000,
+      VRFCoordinator: '0x5C210eF41CD1a72de73bF76eC39637bB0d3d7BEE',
+      VRFKeyHash: '0x9e1344a1247c8a1785d0a4681a27152bffdb43666ae5bf7d14d24a5efd44bf71',
+    },
+
+}
 };
